26628b77b8279005c32e4acbd973cac3c764d19d
Courtney Kimbrough <courtneyekimbrough@gmail.com>
2025-06-09 22:16:25 -0500

    Fix cron

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'https://rubygems.org'
 gem 'selenium-webdriver'
 gem 'webdrivers'
 gem 'mail'
+gem "ostruct"
+gem "base64"
+gem "logger"
+
---
+++ run_track_tickets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Move to your project directory
+cd /Users/courtneykimbrough/repos/ticket_tracker
+
+# Use your exact Ruby and bundler path
+/Users/courtneykimbrough/.rbenv/shims/bundle exec /Users/courtneykimbrough/.rbenv/shims/ruby track_tickets.rb
+
+
